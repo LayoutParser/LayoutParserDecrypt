6ba2af11567131d4e99ca17e13a4ef0c5a080b23
Elson <elsouzalopes@gmail.com>
2025-11-03 16:34:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using LayoutParserLib;
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace LayoutParserDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Environment.Exit(1);
+                return;
+            }
+
+            try
+            {
+                string inputFile = args[0];
+                string outputFile = args[1];
+
+                if (!File.Exists(inputFile))
+                {
+                    Environment.Exit(1);
+                    return;
+                }
+
+                string encryptedContent = File.ReadAllText(inputFile, Encoding.UTF8);
+
+                string contentToDecrypt = encryptedContent;
+                if (encryptedContent.Length > 3)
+                    contentToDecrypt = encryptedContent.Substring(3);
+
+                string decrypted = CryptographySysMiddle.Decrypt(contentToDecrypt);
+
+                File.WriteAllText(outputFile, decrypted, Encoding.UTF8);
+
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("LayoutParserDecrypt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NDD")]
+[assembly: AssemblyProduct("LayoutParserDecrypt")]
+[assembly: AssemblyCopyright("Copyright © NDD 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("2a18f0ee-924d-404b-86b6-e3cec6dabc6d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
